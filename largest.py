8b23d960beecd3ef14bfd25ad8ffb75fe12bbf8a
patilpyash <patilpyash@gmail.com>
2016-10-21 08:59:25 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,8 @@
+print("Program To Find Largest No Amont 2 Nos:")
+print("*"*75)
+a=int(input("Enter The First No:"))
+b=int(input("Enter The Second No:"))
+if a>b:
+    print("The Largest No Is",a)
+else:
+    print("The Largest No Is",b)
